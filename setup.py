cab1e46aa339016a2db96e8c906267a8887f00e1
Shubham Yadav <shubhamyadav2442@gmail.com>
2026-04-02 14:03:16 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A project to predict revenue using an ML model trained on engineered features',
+    author='Shubham',
+    license='MIT',
+)
