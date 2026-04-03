19bd64e18294013dcac4f891357163f83bbdebf1
Shubham Yadav <shubhamyadav2442@gmail.com>
2026-04-03 14:44:55 +0100

    Add: Key based authentication for Dagshub and AWS S3

--- src/models/model_evaluation.py
+++ src/models/model_evaluation.py
@@ -14,11 +14,20 @@ from sklearn.model_selection import LeaveOneOut
 from sklearn.metrics import mean_absolute_error, r2_score
 
 
-# Set tracking uri
-mlflow.set_tracking_uri('https://dagshub.com/shubhamyadav2442/Revenue-predictor-model-trained-on-engineered-features.mlflow')
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-# Initialize dagshub
-dagshub.init(repo_owner='shubhamyadav2442', repo_name='Revenue-predictor-model-trained-on-engineered-features', mlflow=True)
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "shubhamyadav2442"
+repo_name = "Revenue-predictor-model-trained-on-engineered-features"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # Logging configuration
 logger = logging.getLogger('model_evaluation')
